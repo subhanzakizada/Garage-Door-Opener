ad0445e8e7d02d9653ab7f60d7de5549add8e552
Subhan Zaki-Zada <subhanzaki-zada@Subhans-MacBook-Pro.local>
2024-06-20 14:15:58 -0700

    adding user's (myself) info

--- user.js
+++ user.js
@@ -2,6 +2,7 @@
 const users = {
     "1234567": { phone: "1234567", doors: [{ name: "left", status: "open" }, { name: "right", status: "closed" }] },
     "7654321": { phone: "7654321", doors: [{ name: "main", status: "closed" }] },
+    "+12532937820" : { phone: "+12532937820", doors: [ {name: "main", status: "closed"}, {name: "left", status: "closed"}, {name: "right", status: "closed"} ] }
   };
   
 // Function to get user by phone number
