34f2617eae666b76cf102bb282d41052e1fb0504
Eugenio Pace <eugenio.pace@gmail.com>
2024-06-21 13:05:04 -0400

    Factored out sms from app (will do the same for the api in the future

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { parseCommand } = require('./garageDoorControl'); 
+
+const sms = require('./sms');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,51 +17,9 @@ app.get('/', (req, res) => {
     res.send('Welcome to Garage Opener Project');
 });
 
-// Endpoint to handle incoming SMS messages from Twilio
-app.post('/sms', smsHandler);
-app.get('/sms', smsHandler);
-    
-async function smsHandler(req, res){
-    
-    var smsData;
-
-    if(req.method === 'GET'){
-        smsData = req.query;
-    }
-    else if(req.method === 'POST'){
-        smsData = req.body;
-    }
-    else{
-        res.status(405).send('Method Not Allowed');
-        return;
-    }
-    
-    // Extract data from the request body
-    const from = smsData.From;         // Phone number of the sender
-    const body = smsData.Body;         // The SMS message
-    const city = smsData.FromCity;     // City of the sender
-    const state = smsData.FromState;   // State of the sender
-    const country = smsData.FromCountry; // Country of the sender
-    const zip = smsData.FromZip;       // ZIP code of the sender
-
-    console.log('Received SMS:');
-    console.log(`From: ${from}`);
-    console.log(`Message: ${body}`);
-    console.log(`City: ${city}`);
-    console.log(`State: ${state}`);
-    console.log(`Country: ${country}`);
-    console.log(`ZIP: ${zip}`);
- 
-    // Parse the SMS message and get the response
-    const responseMessage = await parseCommand(body, from);
-
-    // Respond to Twilio to acknowledge receipt of the SMS and send the response back to the user
-    res.send(`
-        <Response>
-            <Message>${responseMessage}</Message>
-        </Response>
-    `);
-}
+// App modules
+app.use('/sms', sms);
+//app.use('/api', api);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ sms.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const server = express.Router();
+
+module.exports = server;
+
+const { parseCommand } = require('./garageDoorControl'); 
+
+// Endpoint to handle incoming SMS messages from Twilio
+server.post('/', smsHandler);
+server.get('/', smsHandler);
+    
+async function smsHandler(req, res){
+    
+    var smsData;
+
+    if(req.method === 'GET'){
+        smsData = req.query;
+    }
+    else if(req.method === 'POST'){
+        smsData = req.body;
+    }
+    else{
+        res.status(405).send('Method Not Allowed');
+        return;
+    }
+    
+    // Extract data from the request body
+    const from = smsData.From;         // Phone number of the sender
+    const body = smsData.Body;         // The SMS message
+    const city = smsData.FromCity;     // City of the sender
+    const state = smsData.FromState;   // State of the sender
+    const country = smsData.FromCountry; // Country of the sender
+    const zip = smsData.FromZip;       // ZIP code of the sender
+
+    console.log('Received SMS:');
+    console.log(`From: ${from}`);
+    console.log(`Message: ${body}`);
+    console.log(`City: ${city}`);
+    console.log(`State: ${state}`);
+    console.log(`Country: ${country}`);
+    console.log(`ZIP: ${zip}`);
+ 
+    // Parse the SMS message and get the response
+    const responseMessage = await parseCommand(body, from);
+
+    // Respond to Twilio to acknowledge receipt of the SMS and send the response back to the user
+    res.send(`
+        <Response>
+            <Message>${responseMessage}</Message>
+        </Response>
+    `);
+}
