20364b3b72569dc990fadf0e6c73c943c0bf6d6b
Subhan Zaki-Zada <zakizadasubhan@gmail.com>
2024-06-21 14:45:33 -0700

    Added initial MongoDB setup

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const sms = require('./sms');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// I will change the username/password later and encapsulate it
+const uri = "mongodb+srv://garage-door-opener:garage-door-opener-password@garage-door-opener.begcecd.mongodb.net/?retryWrites=true&w=majority&appName=Garage-Door-Opener"
+
+// Connecting to MongoDB
+async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to MongoDB')
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+connect();
+
 
 // Middleware to parse incoming POST data
 app.use(bodyParser.urlencoded({ extended: false }));
