4d7c93181cdc43de346e677c89d74aa1b3591039
Subhan Zaki-Zada <zakizadasubhan@gmail.com>
2024-06-22 20:15:32 -0700

    Modify to interact with DB directly

--- user.js
+++ user.js
@@ -3,36 +3,30 @@ const { MongoClient } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 
 let dbClient;
-  
+
 async function connect() {
-    try {
+    if (!dbClient) {
         dbClient = new MongoClient(uri);
         await dbClient.connect();
         console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
     }
 }
 
 connect();
 
-// Mock user data
-const users = {
-    "1234567": { phone: "1234567", doors: [{ name: "left", status: "open" }, { name: "right", status: "closed" }] },
-    "7654321": { phone: "7654321", doors: [{ name: "main", status: "closed" }] },
-    "+12532937820" : { phone: "+12532937820", doors: [ {name: "main", status: "closed"}, {name: "left", status: "closed"}, {name: "right", status: "closed"} ] }
-  };
-  
-  // Function to get user by phone number
-  async function getUser(phone) {
-      try {
-          const user = await dbClient.db().collection('users').findOne({ phone });
-          return user;
-      } catch (error) {
-          console.error('Error fetching user:', error);
-          return null;
-      }
-  }
-  
-  module.exports = { getUser };
+// Function to get user by phone number
+async function getUser(phone) {
+    try {
+        const database = dbClient.db();
+        const collection = database.collection('users');
+        const user = await collection.findOne({ phone });
+        return user;
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        return null;
+    }
+}
+
+module.exports = { getUser };
+
   
