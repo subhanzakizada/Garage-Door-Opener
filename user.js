b315b615c116d4c93bd81e9ad6def6527ce6d8c7
Subhan Zaki-Zada <zakizadasubhan@gmail.com>
2024-06-25 04:45:33 -0700

    Change status of door in DB from user's input

--- user.js
+++ user.js
@@ -1,32 +1,45 @@
-require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
-let dbClient;
-
-async function connect() {
-    if (!dbClient) {
-        dbClient = new MongoClient(uri);
-        await dbClient.connect();
-        console.log('Connected to MongoDB');
-    }
-}
-
-connect();
-
-// Function to get user by phone number
 async function getUser(phone) {
     try {
-        const database = dbClient.db();
+        await client.connect();
+        const database = client.db();
         const collection = database.collection('users');
-        const user = await collection.findOne({ phone });
+        const user = await collection.findOne({ phone: phone });
         return user;
     } catch (error) {
-        console.error('Error fetching user:', error);
+        console.error('Error retrieving user:', error);
         return null;
+    } finally {
+        await client.close();
     }
 }
 
-module.exports = { getUser };
+async function updateUser(phone, doorName, status) {
+  try {
+      const client = new MongoClient(process.env.MONGODB_URI);
+      await client.connect();
+      const database = client.db();
+      const collection = database.collection('users');
+
+      const result = await collection.updateOne(
+        { phone: phone }, // Using phone as the selector
+        { $set: { [`doors.$[door].status`]: status } },
+        { arrayFilters: [{ 'door.name': doorName }] }
+    );
+
+  } catch (error) {
+      console.error('Error updating door status:', error);
+  } finally {
+      // Close the client connection
+      if (client) {
+          await client.close();
+      }
+  }
+}
+
+module.exports = { getUser, updateUser };
 
   
