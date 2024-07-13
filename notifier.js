d47c75dfb7e1167575a72efc650dc59567222860
Subhan Zaki-Zada <zakizadasubhan@gmail.com>
2024-07-13 00:28:57 -0700

    Add notifier.js

--- notifier.js
+++ notifier.js
@@ -1,5 +1,32 @@
+require('dotenv').config();
+
+const { getUserByControllerId } = require('./user');
+const twilio = require('twilio');
+const logger = require('./logger');
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = new twilio(accountSid, authToken);
+
 exports.notify = async (door, msg) => {
-    //ToDo: find the user this door belongs to (use controllerId to find it)
-    //Send SMS to the user's phone using Twilio API
-    return;
+    try {
+        // Find the user this door belongs to
+        const user = await getUserByControllerId(door.controllerId);
+        if (!user) {
+            throw new Error(`User not found for controller ID: ${door.controllerId}`);
+        }
+
+        // Send SMS to the user's phone using Twilio API
+        const message = await client.messages.create({
+            body: msg,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            to: user.phone,
+        });
+
+        logger.info(`Message sent to ${user.phone}: ${msg}`);
+        return message;
+    } catch (error) {
+        logger.error('Error in notify:', error);
+        throw error;
+    }
 };
